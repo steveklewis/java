a82143cb19dc9c7eaf8bc0e83c17118da39769e5
Steve Lewis <spiritmech@gmail.com>
2013-03-31 12:01:32 -0400

    First part of maven.apache.org/guides/getting-started/index.html

---
+++ maven/getting-started/my-app/src/main/java/com/mycompnay/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompnay.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
