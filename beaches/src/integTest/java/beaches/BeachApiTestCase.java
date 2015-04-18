f6c194cc0649a144f8df7dabea8d33a2e6d11bc4
steveklewis <lewisksteven@gmail.com>
2015-04-17 23:34:04 -0400

    Setting up integration test for beaches impl in Java.

---
+++ beaches/src/integTest/java/beaches/BeachApiTestCase.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beaches;
+
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+
+/**
+ * 
+ *
+ * @author slewis
+ */
+public class BeachApiTestCase {
+    
+}
