1cbe0249faec0d1c1a3eba996f4c1296811b01a7
steveklewis <lewisksteven@gmail.com>
2016-01-23 08:46:43 -0500

    Remove RestController, use ResponseBody to skip template

--- spring/multiformat/src/main/java/multiformat/Application.java
+++ spring/multiformat/src/main/java/multiformat/Application.java
@@ -3,7 +3,7 @@ package multiformat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class Application {
 
     public static void main(String[] args) {
--- spring/multiformat/src/main/java/multiformat/MultiFormatController.java
+++ spring/multiformat/src/main/java/multiformat/MultiFormatController.java
@@ -1,12 +1,14 @@
 package multiformat;
 
-import java.util.concurrent.atomic.AtomicLong;
+import multiformat.business.GenericGenerator;
+import multiformat.business.IWriter;
+import multiformat.business.WriterFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 
@@ -17,5 +19,17 @@ public class MultiFormatController {
     public String multiformatForm(Model model) {
         model.addAttribute("input", new TestInputBean());
         return "multiformat";
+    }
+    
+    @RequestMapping(value="/multiformat", method=RequestMethod.POST)
+    public @ResponseBody String multiformatSubmit(@ModelAttribute TestInputBean input, Model model) throws Exception {
+        
+        GenericGenerator gen = new GenericGenerator();
+        
+        Class modelClass = BeanFactory.getBeanClass(input.getType());        
+        IWriter writer = WriterFactory.createWriter(input.getFormat());
+        
+        gen.writeFields(modelClass, writer);
+        return writer.toString();
     }
 }
